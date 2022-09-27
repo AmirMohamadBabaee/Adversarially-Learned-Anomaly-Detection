6648cd929250b6b05f4749e20a1aaef5d1ef7795
AmirMohammad Babaei <iran.volleyball79@gmail.com>
2022-09-27 15:16:29 +0330

    add loading mnist dataset

--- utils/adapt_data.py
+++ utils/adapt_data.py
@@ -31,6 +31,22 @@ def adapt_labels_outlier_task(true_labels, label):
         (true_labels[true_labels != label], true_labels[true_labels == label]) = (1, 0)
     return true_labels
 
+def adapt_labels(true_labels, label):
+    """Adapt labels to anomaly detection context
+
+    Args :
+            true_labels (list): list of ints
+            label (int): label which is considered anomalous
+    Returns :
+            true_labels (list): list of labels, 1 for anomalous and 0 for normal
+    """
+    if label == 0:
+        (true_labels[true_labels == label], true_labels[true_labels != label]) = (0, 1)
+        true_labels = [1] * true_labels.shape[0] - true_labels
+    else:
+        (true_labels[true_labels != label], true_labels[true_labels == label]) = (0, 1)
+
+    return true_labels
 
 
 
